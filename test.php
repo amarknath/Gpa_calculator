75f4876c2028800c4284463421e651108580a378
amarknath <amarnathk.k1@gmail.com>
2016-01-26 02:54:39 -0500

    Create test.php

---
+++ test.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html>
+<h1>CGPA Calculator</h1>
+<h3>Grade Obtained</h3> <h3>Credit Hours</h3>
+<body>
+<link rel="stylesheet" type="text/css" href="styled-select.css">
+
+
+<form action="result.php" method="POST"> 
+	<p> Subject 1  
+	              
+  <select name="Subject1">
+    <option val="0">Select</option>
+    <option value="4">A</option>
+    <option value="3.667">A-</option>
+    <option value="3.333">B+</option>
+    <option value="3">B</option>
+    <option value="2.667">B-</option>
+    <option value="2.333">C+</option>
+    <option value="2">C</option>
+    <option value="1.667">C-</option>
+    <option value="0">F</option>
+  </select>
+  <select name="Credits1">
+    <option val="0">Select</option>
+    <option value="4">4</option>
+    <option value="3">3</option>
+    <option value="2">2</option>
+  </select>
+
+</p> 
+<p> Subject 2
+
+  <select name="Subject2">
+    <option val="0">Select</option>
+    <option value="4">A</option>
+    <option value="3.667">A-</option>
+    <option value="3.333">B+</option>
+    <option value="3">B</option>
+    <option value="2.667">B-</option>
+    <option value="2.333">C+</option>
+    <option value="2">C</option>
+    <option value="1.667">C-</option>
+    <option value="0">F</option>
+  </select>
+
+   <select name="Credits2">
+    <option val="0">Select</option>
+    <option value="4">4</option>
+    <option value="3">3</option>
+    <option value="2">2</option>
+  </select>
+
+
+  </p> 
+  <p> Subject 3 
+
+  <select name="Subject3">
+    <option val="0">Select</option>
+    <option value="4">A</option>
+    <option value="3.667">A-</option>
+    <option value="3.333">B+</option>
+    <option value="3">B</option>
+    <option value="2.667">B-</option>
+    <option value="2.333">C+</option>
+    <option value="2">C</option>
+    <option value="1.667">C-</option>
+    <option value="0">F</option>
+  </select>
+
+   <select name="Credits3">
+    <option val="0">Select</option>
+    <option value="4">4</option>
+    <option value="3">3</option>
+    <option value="2">2</option>
+  </select>
+
+
+  </p> 
+  <p> Subject 4
+
+
+<select name="Subject4">
+    <option val="0">Select</option>
+    <option value="4">A</option>
+    <option value="3.667">A-</option>
+    <option value="3.333">B+</option>
+    <option value="3">B</option>
+    <option value="2.667">B-</option>
+    <option value="2.333">C+</option>
+    <option value="2">C</option>
+    <option value="1.667">C-</option>
+    <option value="0">F</option>
+  </select>
+
+   <select name="Credits4">
+    <option val="0">Select</option>
+    <option value="4">4</option>
+    <option value="3">3</option>
+    <option value="2">2</option>
+  </select>
+
+
+  </p> 
+  <p> Subject 5
+
+<select name="Subject5">
+    <option val="0">Select</option>
+    <option value="4">A</option>
+    <option value="3.667">A-</option>
+    <option value="3.333">B+</option>
+    <option value="3">B</option>
+    <option value="2.667">B-</option>
+    <option value="2.333">C+</option>
+    <option value="2">C</option>
+    <option value="1.667">C-</option>
+    <option value="0">F</option>
+  </select>
+
+   <select name="Credits5">
+    <option val="0">Select</option>
+    <option value="4">4</option>
+    <option value="3">3</option>
+    <option value="2">2</option>
+  </select>
+
+
+  </p> 
+  <p> Subject 6
+
+<select name="Subject6">
+    <option val="0">Select</option>
+    <option value="4">A</option>
+    <option value="3.667">A-</option>
+    <option value="3.333">B+</option>
+    <option value="3">B</option>
+    <option value="2.667">B-</option>
+    <option value="2.333">C+</option>
+    <option value="2">C</option>
+    <option value="1.667">C-</option>
+    <option value="0">F</option>
+  </select>
+
+   <select name="Credits6">
+    <option val="0">Select</option>
+    <option value="4">4</option>
+    <option value="3">3</option>
+    <option value="2">2</option>
+  </select>
+
+
+  </p> 
+  <p> Subject 7
+
+<select name="Subject7">
+    <option val="0">Select</option>
+    <option value="4">A</option>
+    <option value="3.667">A-</option>
+    <option value="3.333">B+</option>
+    <option value="3">B</option>
+    <option value="2.667">B-</option>
+    <option value="2.333">C+</option>
+    <option value="2">C</option>
+    <option value="1.667">C-</option>
+    <option value="0">F</option>
+  </select>
+
+   <select name="Credits7">
+    <option val="0">Select</option>
+    <option value="4">4</option>
+    <option value="3">3</option>
+    <option value="2">2</option>
+  </select>
+  </div></div>
+
+  </p> 
+
+
+  <input type="submit" value="CGPA>>" >
+</form>
+
+</body>
+</html>
